b098ec1e12618ad065054939e3e9ce0568e6d635
Max <maksim.chernyshev@gmail.com>
2021-11-15 20:31:19 +0200

    first commit

---
+++ resources/views/front/layouts/parts/footer.blade.php
@@ -0,0 +1,20 @@
+<footer class="main-footer fixed-bottom">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                FOOTER
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                <strong>Copyright © {{ now()->year }} <a href="https://maksimchernyshev.com" target="_blank">Max Chernyshev</a>.</strong> All rights reserved.
+            </div>
+        </div>
+    </div>
+</footer>
+<script src="{{ asset('js/front/js/app.js') }}"></script>
+
+</body>
+</html>
